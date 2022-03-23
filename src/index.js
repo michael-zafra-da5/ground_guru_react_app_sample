254a13da1328c4dfad2ab21eaa1f2c7ebdcab2a3
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-03-23 13:14:37 +0800

    - Added Page not found - Added Router setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import NotFound from './components/NotFound';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <Link to="/">Home</Link> */}
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="*" element={<NotFound />}/>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
