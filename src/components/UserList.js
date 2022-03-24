7e945729c5c498d88623b68274f6393e9d937759
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-03-24 11:20:46 +0800

    - Implemented Material UI for display of user

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,45 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+
 export const UserList = ({ data }) => 
 <ul>
     {
         data !== undefined ? data.map(item => 
-            Object.keys(item).map((key,index) => 
-                <p>{item[key]}</p>
-            )
+            // Object.keys(item).map((key,index) => 
+            //     // <p>{item[key]}</p>
+            // )
+            <Card
+                style={{
+                width: 400,
+                backgroundColor: item['status'] === 'active' ? "yellow" : "gray",
+                marginTop: 25
+                }}
+            >
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                        {item["name"]}
+                    </Typography>
+                    <Typography
+                        color="textSecondary"
+                    >
+                        {item["email"]}
+                    </Typography>
+                    <Typography
+                        style={{
+                        marginBottom: 12,
+                        }}
+                        color="textSecondary"
+                    >
+                        Gender: {item["gender"]}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">View Details</Button>
+                </CardActions>
+            </Card>
         ) : ''
     }
 </ul>
