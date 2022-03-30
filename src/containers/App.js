aa4ae9299157e478cf1d58376c5827f782b8e8bd
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-03-30 13:13:59 +0800

    - Updated base App setup - Implemented themes - Added dashboard page - Added navbar and sidebar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,121 +1,46 @@
-import '../App.css';
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { createUser, fetchUsers } from "../actions/index";
-import { UserList } from '../components/UserList'
-import { BarLoader } from 'react-spinner-animated';
-import 'react-spinner-animated/dist/index.css';
-import { useForm } from 'react-hook-form';
-import { FETCH_API_DATA, API_ERROR, CREATE_USER } from "../actions/types";
-import { Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText } from '@mui/material';
-import { Button } from "@material-ui/core";
+import { CacheProvider } from '@emotion/react';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { createEmotionCache } from '../utils/create-emotion-cache';
+import { theme } from '../theme';
+import Login from '../containers/login';
+import User from '../containers/User';
+import Register from '../containers/register';
+import NotFound from '../components/NotFound';
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
+import { DashboardLayout } from '../components/dashboard-layout';
 
-function App() {
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.apiReducer.data);
-  const dataType = useSelector(state => state.apiReducer.type);
-  const apiLoading = useSelector(state => state.apiReducer.isLoadingData);
-  const error = useSelector(state => state.apiReducer.error);
+const clientSideEmotionCache = createEmotionCache();
 
-  const { register, handleSubmit, formState : { errors } } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-    dispatch(createUser(data))
-  }; 
+const App = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-  const [isLoaded, setLoaded] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [dialogMessage, setDialogMessage] = useState('');
-  const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-  const handleClose = () => {
-      setOpen(false);
-  };
-
-  useEffect(() => {
-      if (!isLoaded) {
-        console.log('initial loading');
-        setLoaded(!isLoaded);
-        dispatch(fetchUsers());
-      }
-
-      if (apiLoading !== false && dataType === CREATE_USER) {
-        dispatch(fetchUsers());
-      }
-
-      console.log('data:'+data);
-      if (!open && error !== undefined && data === undefined && dataType === API_ERROR) {
-        console.log('error '+error);
-        setDialogMessage(error.message);
-        handleClickOpen()
-        dispatch(fetchUsers());
-      }
-  }, [isLoaded, apiLoading, dataType, data, open, error, dispatch]);
+  // const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        { apiLoading === true ? 
-          <BarLoader text={"Loading..."} center={false} width={"150px"} height={"150px"}/> 
-        : 
-          <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <label>
-                  Name:
-                <input defaultValue="" {...register("name", { required: true })}/>
-              </label>
-              {errors.name && <span>This field is required</span>}
-
-              <label>
-                  Email:
-                <input defaultValue="" {...register("email", { required: true })}/>
-              </label>
-              {errors.email && <span>This field is required</span>}
-
-              <label>
-                  Gender:
-                <input defaultValue="male" {...register("gender", { required: true })}/>
-              </label>
-              {errors.gender && <span>This field is required</span>}
-
-              <label>
-                  Status:
-                <input defaultValue="active" {...register("status", { required: true })}/>
-              </label>
-              {errors.status && <span>This field is required</span>}
-
-              <input type="submit" />
-            </form>
-
-            <button onClick={() => dispatch(fetchUsers())}>Refresh User List</button>
-            {data !== undefined && dataType === FETCH_API_DATA ? <UserList data={data}></UserList> : ''}
-          </div>
-        }
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogContent>
-            <DialogTitle id="alert-dialog-title">
-              {"Invalid"}
-            </DialogTitle>
-            <DialogContentText id="alert-dialog-description">
-              {dialogMessage !== '' ? dialogMessage : 'Something went wrong.'}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Ok</Button>
-          </DialogActions>
-        </Dialog>
-      </header>
-      
-
-    </div>
+    <CacheProvider value={emotionCache}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* {getLayout(<Component {...pageProps} />)} */}
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/user/:id" element={<User />}/>
+                <Route path="/aboutUs" element={<App />}/>
+                <Route path="/register" element={<Register />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/home" element={<DashboardLayout />}/>
+                <Route path="*" element={<NotFound />}/>
+                <Route />
+              </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+      </LocalizationProvider>
+    </CacheProvider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import User from './containers/User';
-import Register from './containers/register';
-import NotFound from './components/NotFound';
+import configureStore from './stores/index';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import configureStore from './stores/index'
-import Login from './containers/login';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}/>
-          <Route path="/user/:id" element={<User />}/>
-          <Route path="/aboutUs" element={<App />}/>
-          <Route path="/register" element={<Register />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="*" element={<NotFound />}/>
-          <Route />
-        </Routes>
-      </BrowserRouter>
+      <App/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
