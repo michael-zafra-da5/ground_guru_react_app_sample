705e0b14e0f8ad671a73d22e930ac7fdb3203830
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-04-04 09:40:54 +0800

    - Implemented localstorage in state thru middleware

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,5 +3,10 @@ import { rootReducer } from "../reducers";
 import { apiMiddleware } from "../middleware/api";
 import {composeWithDevTools} from 'redux-devtools-extension';
 
-const configureStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(apiMiddleware)));
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
+const configureStore = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(apiMiddleware)));
+configureStore.subscribe(() =>
+    localStorage.setItem('reduxState', JSON.stringify(configureStore.getState()))
+);
 export default configureStore;
